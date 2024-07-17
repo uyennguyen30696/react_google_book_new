cba58c0e384020d1e45a6478e035c3e837ffcda8
Uyen Nguyen <nguyenuyen30696@gmail.com>
2024-07-17 12:47:29 -0700

    first commit, boilerplate mvc model

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p>test</p>
+      </div>
+    </>
+  )
+}
+
+export default App
